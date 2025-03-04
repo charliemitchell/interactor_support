00b704c1432fc397b2132b521a5b78460e885f58
Charlie Mitchell <charlie@darkbarcrew.com>
2025-03-03 19:27:06 -0700

    fix: linting

--- lib/interactor_support.rb
+++ lib/interactor_support.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
-require "active_support/concern"
-require "interactor_support"
+require 'active_support/concern'
+require 'interactor_support'
 
-require_relative "interactor_support/version"
-require_relative "interactor_support/actions"
-require_relative "interactor_support/validations"
-require_relative "interactor_support/request_object"
-Dir[File.join(__dir__, "interactor_support/concerns/*.rb")].sort.each { |file| require file }
+require_relative 'interactor_support/version'
+require_relative 'interactor_support/actions'
+require_relative 'interactor_support/validations'
+require_relative 'interactor_support/request_object'
+Dir[File.join(__dir__, 'interactor_support/concerns/*.rb')].sort.each { |file| require file }
+
+# Conditionally load RuboCop cops if RuboCop is installed
+begin
+  require 'rubocop'
+  require_relative 'interactor_support/rubocop/cop/require_required_for_interactor_support'
+rescue LoadError
+  # RuboCop is not installed, so we don't load the cop
+end
 
 module InteractorSupport
   extend ActiveSupport::Concern
--- lib/interactor_support/validations.rb
+++ lib/interactor_support/validations.rb
@@ -1,4 +1,4 @@
-require "active_model"
+require 'active_model'
 
 module InteractorSupport
   module Validations
@@ -8,54 +8,56 @@ module InteractorSupport
       include ActiveModel::Validations
       include ActiveModel::Validations::Callbacks
 
-      def self.optional(*keys)
-        context_accessor(*keys)
-      end
+      class << self
+        def optional(*keys)
+          context_accessor(*keys)
+        end
 
-      def self.validates_after(*keys, **validations)
-        after do
-          keys.each do |key|
-            validation_for_presence(key) if validations[:presence]
-            validation_for_inclusion(key, validations[:inclusion]) if validations[:inclusion]
-            validation_for_persistence(key) if validations[:persisted]
-            validation_for_type(key, validations[:type]) if validations[:type]
+        def validates_after(*keys, **validations)
+          after do
+            keys.each do |key|
+              validation_for_presence(key) if validations[:presence]
+              validation_for_inclusion(key, validations[:inclusion]) if validations[:inclusion]
+              validation_for_persistence(key) if validations[:persisted]
+              validation_for_type(key, validations[:type]) if validations[:type]
+            end
           end
         end
-      end
 
-      def self.validates_before(*keys, **validations)
-        before do
-          context.fail!(errors: ["persisted validation is only available for after validations"]) if validations[:persisted]
+        def validates_before(*keys, **validations)
+          before do
+            context.fail!(errors: ['persisted validation is only available for after validations']) if validations[:persisted]
 
-          keys.each do |key|
-            validation_for_presence(key) if validations[:presence]
-            validation_for_inclusion(key, validations[:inclusion]) if validations[:inclusion]
-            validation_for_type(key, validations[:type]) if validations[:type]
+            keys.each do |key|
+              validation_for_presence(key) if validations[:presence]
+              validation_for_inclusion(key, validations[:inclusion]) if validations[:inclusion]
+              validation_for_type(key, validations[:type]) if validations[:type]
+            end
           end
         end
-      end
 
-      # Define context accessors dynamically
-      def self.context_accessor(*keys)
-        keys.each do |key|
-          attr_accessor(key)
+        # Define context accessors dynamically
+        def context_accessor(*keys)
+          keys.each do |key|
+            attr_accessor(key)
 
-          define_method(key) { context[key] }
-          define_method("#{key}=") { |value| context[key] = value }
-        end
-      end
-
-      def self.required(*keys)
-        keys.each do |key|
-          attr_accessor(key)
-
-          define_method(key) { context[key] }
-          define_method("#{key}=") { |value| context[key] = value }
+            define_method(key) { context[key] }
+            define_method("#{key}=") { |value| context[key] = value }
+          end
         end
 
-        before do
-          missing_keys = keys - context.to_h.keys
-          context.fail!(errors: missing_keys.map { |key| "#{key} is required" }) if missing_keys.any?
+        def required(*keys)
+          keys.each do |key|
+            attr_accessor(key)
+
+            define_method(key) { context[key] }
+            define_method("#{key}=") { |value| context[key] = value }
+          end
+
+          before do
+            missing_keys = keys - context.to_h.keys
+            context.fail!(errors: missing_keys.map { |key| "#{key} is required" }) if missing_keys.any?
+          end
         end
       end
     end
@@ -67,11 +69,11 @@ module InteractorSupport
     end
 
     def validation_for_inclusion(key, inclusion)
-      raise ArgumentError, "inclusion validation requires an inclusion hash" unless inclusion.is_a?(Hash)
-      raise ArgumentError, "inclusion validation requires an :in key" unless inclusion[:in].present?
+      raise ArgumentError, 'inclusion validation requires an inclusion hash' unless inclusion.is_a?(Hash)
+      raise ArgumentError, 'inclusion validation requires an :in key' unless inclusion[:in].present?
 
       raise ArgumentError,
-"inclusion validation requires an array or range of values" unless inclusion[:in].is_a?(Array) || inclusion[:in].is_a?(Range)
+'inclusion validation requires an array or range of values' unless inclusion[:in].is_a?(Array) || inclusion[:in].is_a?(Range)
 
       context.fail!(errors: ["#{key} was not in the specified inclusion"]) unless inclusion[:in].include?(context[key])
     rescue ArgumentError => e
@@ -85,7 +87,7 @@ module InteractorSupport
     def validation_for_persistence(key)
       validation_for_presence(key)
       raise ArgumentError,
-      "persisted validation requires the context key to be a model" unless context[key].is_a?(ApplicationRecord)
+      'persisted validation requires the context key to be a model' unless context[key].is_a?(ApplicationRecord)
 
       context.fail!(errors: ["#{key} was not persisted"] + context[key].errors.full_messages) unless context[key].persisted?
     end
